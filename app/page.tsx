3185c7ac0626233a7a97745662bb3f3748337c1d
Naruê Gonçalves Santos <naruegsantos@gmail.com>
2024-11-30 21:21:03 -0300

    feat: add todos

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main className="p-4">
-    <h1>Geek Bee <strong>lms</strong></h1>
-    <div className="pl-4">
-      |_<span className="underline">Cycles</span>
+   <>
+    <main className="p-4">
+      <h1>Geek Bee <strong>lms</strong></h1>
       <div className="pl-4">
-        |_<span className="underline">Modules</span>
+        |_<span className="underline">Cycles</span>
         <div className="pl-4">
-          |_<span className="underline">Steps</span>
+          |_<span className="underline">Modules</span>
+          <div className="pl-4">
+            |_<span className="underline">Steps</span>
+          </div>
         </div>
       </div>
-    </div>
-   </main>
+    </main>
+    <footer className="mt-10">
+      <h1>todos</h1>
+      everething
+    </footer>
+   </>
   );
 }
